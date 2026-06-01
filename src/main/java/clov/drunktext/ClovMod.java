bfdd3f1c8144f74e6ee34ca2a0785c9b2943b8a2
cloverskid <arthur1999enale@gmail.com>
2026-06-01 22:01:05 +0600

    first commit (1.21.11)

---
+++ src/main/java/clov/drunktext/ClovMod.java
@@ -0,0 +1,23 @@
+package clov.drunktext;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClovMod implements ModInitializer {
+	public static final String MOD_ID = "brewery-drunktext";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+    	LOGGER.info("Drunk chat enabled");
+		MakeDrunkText.initialize();
+	}
+}
